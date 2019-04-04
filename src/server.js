a0c726bbbdcb2d51881b76fd5e10b67e7293c517
Brandon Kim <bkim2490@gmail.com>
2019-04-04 17:59:56 +0900

    renamed client folder

--- src/server.js
+++ src/server.js
@@ -2,8 +2,8 @@ const app = require('./app');
 const http = require('http');
 const server = http.createServer(app);
 
-server.listen(3000);
+server.listen(5000);
 
 server.on('listening', () => {
-    console.log('server is listening on port 3000');
+    console.log('server is listening on port 5000');
 })
